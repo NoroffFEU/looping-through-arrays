098259dcf3336139eaa347bc97ece9842f7792ec
Connor O'Brien <connor@hifranc.com>
2020-12-31 22:47:10 +0200

    Add script file

---
+++ script.js
@@ -0,0 +1,25 @@
+// example 1
+var list = ["bananas", "a few frogs", "some rats", "rat poison", "cow"];
+
+// var len = list.length;
+
+// console.log(len);
+
+for (var i = 0; i < list.length; i++) {
+
+    console.log(i);
+    var item = list[i];
+
+    console.log(item);
+}
+
+
+
+// example 2
+var colours = ["red", "blue", "green", "orange", "pink"];
+
+for (var i = 0; i < colours.length; i++) {
+
+    var col = colours[i];
+    console.log(col);
+}
